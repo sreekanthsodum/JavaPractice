6d296cd136084de50f51af951c00f8f50ac21fe2
Sodum Sreekanth <sreekanth.te@gmail.com>
2025-06-20 13:58:26 +0530

    Removed Unnecessary Files.

--- Array/Array1.java
+++
@@ -1,6 +0,0 @@
-class Array1 {
-	public static void main(String[] args) {
-		int arr1[5] = {1,2,3,4,5};
-		System.out.print(arr1[3]);
-	}
-}
---
+++ Array/Sreekanth.java
@@ -0,0 +1,8 @@
+package Array;
+public class Sreekanth {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5};
+        System.out.print(arr1[3]);
+    }
+    
+}
